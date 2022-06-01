d9e986e56911136520a7f7bcea0ab64aa9fb3587
Jyrki Mottonen <jyrki.mottonen@helsinki.fi>
2022-06-01 19:10:38 +0300

    New arguments fpen and lpen.

--- R/fusedladlasso.R
+++ R/fusedladlasso.R
@@ -2,10 +2,18 @@
 #'
 #' \code{fusedladlasso} is used to fit the multivariate fused LAD-lasso regression model. 
 #'
-#' @param Y an nxq matrix
-#' @param X an nxp matrix
+#' @param Y an nxq matrix of responses. The ith row contains the q-variate response
+#' of the ith individual.
+#' @param X an nxp matrix of p explaining variables, The ith row contains the values
+#' of p explaining variables for the ith individual.
 #' @param lambda1 the tuning parameter for the LAD-penalty
 #' @param lambda2 the tuning parameter for the fusion-penalty
+#' @param lpen gives the lasso penalized coefficients. For example, lpen=c(2,5:8)
+#' means that the coefficient vectors beta2, beta5,...,beta8 are penalized.
+#' @param fpen a list of blocks of fusion penalized coefficients. For example, 
+#' fpen=list(2:5,10:20) means that the fusion penalty is 
+#' \code{lambda2*[||beta_3-beta_2||+...+||beta_5-beta_4||+
+#' ||beta_{11}-beta_{10}||+...+||beta_{20}-beta_{19}||]}
 #' @details 
 #' Here are the details of the function...
 #' @return A list containing the following components:
@@ -32,49 +40,74 @@
 #' out$runtime
 #' }
 #' @export
-fusedladlasso<-function(Y,X,lambda1=0,lambda2=0)
+fusedladlasso<-function(Y, X, lambda1=0, lambda2=0, 
+                        lpen=1:dim(X)[2], fpen=list(1:dim(X)[2]))
 {
   if(is.data.frame(Y))Y<-as.matrix(Y)
   if(is.data.frame(X))X<-as.matrix(X)
   if(dim(Y)[2]<2)stop("response should be at least 2-dimensional!")
   if(dim(X)[1]!=dim(Y)[1])stop("response matrix Y and design matrix X
                                  should have equal number of rows!")
+  nblocks<-length(lengths(fpen)) #The number of blocks
+  conc<-NULL
+  for(blk in 1:nblocks)conc<-c(conc,fpen[[blk]])
+  if(anyDuplicated(conc)!=0)stop("blocks: the blocks should not overlap!")
+  isect<-intersect(1:dim(X)[2],conc)
+  if(length(isect)<length(conc))
+    stop(paste("the blocks should contain only integers from 1 to ",dim(X)[2],sep=""))
   warn.init<-options()$warn
   options(warn=-1)
-  RowNorms <- function(X)
+  q<-ncol(Y)     #The number of traits
+  p<-ncol(X)     #The number of explaining variables
+  n<-nrow(Y)     #The number of cases   
+  lpen<-sort(lpen)
+  fused.id<-NULL
+  for(i in 1:nblocks)
   {
-    sqrt(rowSums2(X^2))
+    id<-min(fpen[[i]]):(max(fpen[[i]])-1)
+    fused.id<-c(fused.id,id)
   }
-  p<-ncol(Y)     #The number of traits
-  X<-cbind(1,X)  #Add the intercept term
-  q<-ncol(X)     #The number of explaining variables + 1
-  n<-nrow(Y)     #The number of cases    
+  fused.id<-sort(fused.id)
   if((lambda1==0)&(lambda2>0))
   {
-    Y1<-matrix(0,q-2,p) 
-    y<-rbind(Y,Y1)   
-    X2<-n*lambda2*(diag(q)[-c(1,2),]-diag(q)[-c(1,q),])
+    Y2<-matrix(0,p-1,q) 
+    Y2<-Y2[fused.id,]
+    y<-rbind(Y,Y2)
+    W<-cbind(0,diag(p-1))-cbind(diag(p-1),0)
+    X<-cbind(1,X)
+    X2<-cbind(0,n*lambda2*W)
+    X2<-X2[fused.id,]
     x<-rbind(X,X2)  
   }
   else if((lambda1>0)&(lambda2==0))
   {
-    Y1<-matrix(0,q-1,p) 
+    Y1<-matrix(0,p,q)
+    Y1<-Y1[lpen,]
     y<-rbind(Y,Y1)   
-    X1<-n*lambda1*diag(q)[-1,]
+    X<-cbind(1,X)
+    X1<-cbind(0,n*lambda1*diag(p))
+    X1<-X1[lpen,]
     x<-rbind(X,X1)  
   }
   else if((lambda1>0)&(lambda2>0))
   {
-    Y1<-matrix(0,2*q-3,p) 
-    y<-rbind(Y,Y1)   
-    X1<-n*lambda1*diag(q)[-1,]
-    X2<-n*lambda2*(diag(q)[-c(1,2),]-diag(q)[-c(1,q),])
+    Y1<-matrix(0,p,q) 
+    Y1<-Y1[lpen,]
+    Y2<-matrix(0,p-1,q) 
+    Y2<-Y2[fused.id,]
+    y<-rbind(Y,Y1,Y2)   
+    X<-cbind(1,X)
+    X1<-cbind(0,n*lambda1*diag(p))
+    X1<-X1[lpen,]
+    W<-cbind(0,diag(p-1))-cbind(diag(p-1),0)
+    X2<-cbind(0,n*lambda2*W)
+    X2<-X2[fused.id,]
     x<-rbind(X,X1,X2)  
   }
   else if((lambda1==0)&(lambda2==0))
   {
     y<-Y
-    x<-X
+    x<-cbind(1,X)
   }
   else
     stop("lambda1 and lambda2 should be non-negative numbers")
